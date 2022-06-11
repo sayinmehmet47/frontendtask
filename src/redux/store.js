abc30a97960b125bf1d2d8860715c4f463b52e91
mehmet <sayinmehmet47@mehmets-MacBook-Air.local>
2022-06-11 17:44:16 +0200

    fix(client-web): added comment

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import historySlice from './historySlice';
-//deneme
+//deneme1234
 export const store = configureStore({
   reducer: {
     history: historySlice,
