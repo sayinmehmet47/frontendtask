d781a95d5dc8206c1ead253d3145e7528a791a0e
mehmet <sayinmehmet47@gmail.com>
2022-06-12 14:02:50 +0200

    conflict resolved

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import historySlice from './historySlice';
 //deneme
 
 //deneme2
+<<<<<<< HEAD
 //deneme3
+=======
+//deneme4
+>>>>>>> bbdf80b (added text)
 export const store = configureStore({
   reducer: {
     history: historySlice,
