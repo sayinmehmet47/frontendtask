a3e17a06aee21770dd9277f5a6a51d6024d156db
mehmet sayin <75525090+sayinmehmet47@users.noreply.github.com>
2022-06-12 12:13:24 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import historySlice from './historySlice';
 //anlamadim
 //duzeltme
-
+//edit2
 export const store = configureStore({
   reducer: {
     history: historySlice,
