b3d7dc72b021e0dc8557ab6eed0ce0fedc35c5e3
mehmet <sayinmehmet47@gmail.com>
2022-06-12 14:15:45 +0200

    upgrade added

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import historySlice from './historySlice';
 //deneme
 //gelistirme
-
+//upgrade2
 export const store = configureStore({
   reducer: {
     history: historySlice,
