10b781ccaa999e79c967cc111aaa8a350fe51a22
mehmet sayin <75525090+sayinmehmet47@users.noreply.github.com>
2022-06-12 14:06:52 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import historySlice from './historySlice';
-//deneme
 
-//deneme2
-//deneme3
 export const store = configureStore({
   reducer: {
     history: historySlice,
