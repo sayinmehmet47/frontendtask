1991aa74192ed63f4fd028059bc4c99d34f7f9fd
mehmet <sayinmehmet47@mehmets-MacBook-Air.local>
2022-06-12 12:14:49 +0200

    edit2 fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import historySlice from './historySlice';
 //anlamadim
 //duzeltme
+//edit2
 
 export const store = configureStore({
   reducer: {
