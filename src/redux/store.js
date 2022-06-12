1b3248efb10e46ae8749f1fb525678b1a9941b06
mehmet <sayinmehmet47@gmail.com>
2022-06-12 13:08:22 +0200

    conflicts resolved

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import historySlice from './historySlice';
 //deneme
-<<<<<<< HEAD
 
 //deneme2
-=======
-//deneme1
->>>>>>> master
 export const store = configureStore({
   reducer: {
     history: historySlice,
