56bacb7c5f0433463130895acd8d7bb2f4f40c72
mehmet <sayinmehmet47@gmail.com>
2022-06-12 15:58:34 +0200

    comments deleted

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import historySlice from './historySlice';
-//deneme
-//gelistirme
-//upgrade2
+
 export const store = configureStore({
   reducer: {
     history: historySlice,
